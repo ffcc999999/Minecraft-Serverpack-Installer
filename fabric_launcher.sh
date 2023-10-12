0008715aff2d1fdd6eebbd22bb70c9b576c6c17c
ffcc999999 <119822454+ffcc999999@users.noreply.github.com>
2023-10-12 14:14:10 +0200

    Update fabric_launcher.sh

--- fabric_launcher.sh
+++ fabric_launcher.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Check if "variables.txt" exists
-if [[ -f "variables.txt" ]]; then
+if [ -f "variables.txt" ]; then
     # Use awk to create variables
     while IFS='=' read -r key value; do
         declare "$key=$value"
     done < "variables.txt"
     
     # Check if MODLOADER is "Fabric"
-    if [[ "$MODLOADER" == "Fabric" ]]; then
+    if [ "$MODLOADER" = "Fabric" ]; then
         # Define the URL to download the jar file
         fabric_url="https://meta.fabricmc.net/v2/versions/loader/$MINECRAFT_VERSION/$MODLOADER_VERSION/$FABRIC_INSTALLER_VERSION/server/jar"
         
@@ -16,7 +16,7 @@ if [[ -f "variables.txt" ]]; then
         http_status=$(curl -w "%{http_code}" -o "fabric-server-launch.jar" "$fabric_url" -s)
         
         # Check if the download is successful
-        if [[ "$http_status" -eq 200 ]]; then
+        if [ "$http_status" -eq 200 ]; then
             echo "File downloaded as fabric-server-launch.jar"
         else
             echo "Failed to download file: $http_status"
@@ -29,8 +29,9 @@ else
 fi
 
 # If the file fabric-server-launch.jar exists, rename it to server.jar
-if [[ -f "fabric-server-launch.jar" ]]; then
-    mv "fabric-server-launch.jar" "server.jar"
+if [ -f "fabric-server-launch.jar" ]; then
+    rm -rf server.jar
+    mv fabric-server-launch.jar server.jar
     echo "Renamed fabric-server-launch.jar to server.jar"
 else
     echo "An error occurred: fabric-server-launch.jar not found."
